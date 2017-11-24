7a47087a1cc1cfd0d6ca616066275fe5d2ca8a2e
Juuso Hynynen <33858850+JHenrik@users.noreply.github.com>
2017-11-24 15:39:10 +0200

    These are the scripts for scene manager
    Test.cs is to be placed on Scene1's Scenemanager
    Test2.cs is to be placed on Scene2's Scenemanager

---
+++ Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour {
+	
+		private Button Button1;
+		private Button Button2;
+
+
+		// Use this for initialization
+		void Start () {
+		
+			Debug.Log ("start");
+
+			Button1 = GameObject.Find ("Button1").GetComponent<Button> ();
+			Button1.onClick.AddListener (() => changeRoom ("Forward"));
+
+
+		}
+		private void changeRoom (string Direction){
+			Debug.Log ("move");
+			SceneManager.LoadScene ("TutoraidAssets/Scene2");
+
+
+		}
+	}
---
+++ Assets/Test2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Test2 : MonoBehaviour {
+
+	private Button Button1;
+	private Button Button2;
+
+	//private RawImage backGroundPicture;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("start");
+
+		Button1 = GameObject.Find ("Button1").GetComponent<Button> ();
+		Button1.onClick.AddListener (() => changeRoom ("Forward"));
+
+
+	}
+	private void changeRoom (string Direction){
+		Debug.Log ("move");
+		SceneManager.LoadScene ("TutoraidAssets/Scene3");
+
+
+	}
+}
