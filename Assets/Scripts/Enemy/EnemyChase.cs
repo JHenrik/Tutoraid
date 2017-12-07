70829f9d51bb2cb1013f3242c6575430fc1ee63b
gbenarh <gbenarh@gmail.com>
2017-12-07 21:43:40 +0200

    EnemyAI now working

---
+++ Assets/Scripts/Enemy/EnemyChase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour 
+{
+	
+	[SerializeField]
+	private Transform player; // reference to the player as a target
+	[SerializeField]
+	private Transform restart; // reference to a patrol target
+	//public GameObject Enemy;
+	private GameObject playerObject;
+
+	private EnemyMovement enemyMovement; //..holder for the enemymovement script
+
+void Start ()
+{
+
+	playerObject = GameObject.Find("Player");
+	enemyMovement = GetComponent<EnemyMovement>();
+}
+
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "Player")
+		{
+			enemyMovement.currentTarget = playerObject.transform;
+			
+			enemyMovement.enemySpeed = 3;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col)
+	{
+		if (col.tag == "Player")
+		{
+			
+			enemyMovement.currentTarget = restart.transform;
+			enemyMovement.enemySpeed = 2;
+		}
+	}	
+
+
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform targetOne;   //..patrol target one
+
+	[SerializeField]
+	private Transform targetTwo;  //..patrol target two
+
+	public Transform currentTarget;  //..current target enemy is moving towards
+	private float move;
+	public float directionX;
+	
+	public float Step;
+	
+	private Animator enemyAC;
+
+
+	public float enemySpeed = 2;
+
+	void Start ()
+ 	{
+		directionX = 0.0f;
+		currentTarget = targetOne;
+		enemyAC = GetComponent<Animator> ();
+
+	}
+
+	void FixedUpdate () 
+	{
+
+		if(Vector3.Distance(transform.position, currentTarget.position) < 0.1f)
+		{
+			Switch ();
+		}
+
+		Movement (); 	
+		
+	}
+
+	void Movement ()
+	{
+		//..asigning the move speed.
+		move = enemySpeed * Time.deltaTime;
+
+		//..moving the enemy towards the target at a given speed
+		transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, move);
+
+		directionX = currentTarget.position.x - transform.position.x;
+
+		//Player Direction that is needed to flip the sprite
+		if (directionX < 0.0f)
+		{
+	    	transform.localScale = new Vector3 (-1, 1, 1);
+		}
+		else if (directionX > 0.0f)
+		{
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		 Step = Mathf.Abs(directionX);
+		if (Step > 0.0f)
+		{
+			enemyAC.SetBool ("Walking", true);
+		}else
+		{
+			enemyAC.SetBool ("Walking", false);		
+		}
+	}
+
+
+	void Switch () 
+	{
+		//..Switching the target points.
+		if(currentTarget == targetOne)
+		{
+			currentTarget = targetTwo;
+		}
+		else
+		{
+			currentTarget = targetOne;
+		}
+	}
+}
